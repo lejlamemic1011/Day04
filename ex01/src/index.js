6531dcd21e9f8bc2a048806cf66b5838a360c44e
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 18:19:43 +0100

    Update index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -8,15 +8,15 @@ function main(numOne, numTwo){
          // Only change code below this line 
 var numOne;
 var numTwo; 
-var quotient = numOne/numTwo; 
+var quotient = numOne / numTwo; 
          // Only change code above this line 
 
     }
 
     return quotient;
 }
-
-console.log(main(25, 5)); 
-console.log(main(420, 10)); 
+console.log(main(15, 3));     //change this line 
+console.log(main(25, 5));     //change this line 
+console.log(main(420, 10));    //change this line 
 console.log(main(55, 0));     //change this line 
 module.exports = main; 
