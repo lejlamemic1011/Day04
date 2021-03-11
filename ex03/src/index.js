bdfdcec9f74e096cad91cc4ebd2c1338162514fb
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 19:44:33 +0100

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -3,7 +3,7 @@ function main(){
     var myRes = 16;
     
     // Only change code below this line 
-    myRes = --;  
+    myRes--;  
     // Only change code above this line 
 
     return myRes;
