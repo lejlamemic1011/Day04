2be03db7a84fcc4402f487ec6620fd0b0c790418
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 20:02:15 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,16 @@
+function main() {
+    // Only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 0.00254;
+    var myDecimalE = myDecimalE.toExponential();
 
+    // Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
