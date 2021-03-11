bf28d2dcdc0816cffa16c6fc1533337aa26ad822
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 19:44:00 +0100

    Update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -3,7 +3,7 @@ function main(){
     var myRes = 75;
     
     // Only change code below this line 
-    myRes = ++;  
+    myRes++;  
     // Only change code above this line 
 
     return myRes;
