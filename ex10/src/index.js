9b518f87b3dc24a44180d420c60f41c246fff3b8
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 20:08:30 +0100

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,17 @@
+function main() {
+    // Only change code below this line
+    var myFistName = "Adin";
+    var myLastName = "Hadzovic";
+    var myHomeTown = "Sarajevo";
 
+    // Only change code above this line
+
+    return {
+        myFistName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
