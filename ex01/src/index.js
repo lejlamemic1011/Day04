c94400527cc1b8840009c3e52ce32c8299a78c6d
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 11:09:06 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,22 @@
+function main(numOne, numTwo){
 
+    var quotient;
+    
+    if(numTwo === 0){
+        quotient = Divide by 0! Cannot divide by zero! ;
+    } else {
+         // Only change code below this line 
+var numOne;
+var numTwo; 
+var quotient = numOne/numTwo; 
+         // Only change code above this line 
+
+    }
+
+    return quotient;
+}
+
+console.log(main(25, 5)); 
+console.log(main(420, 10)); 
+console.log(main(55, 0));     //change this line 
+module.exports = main; 
