8177ecc93157c514d6341c6ccdbfc9e705c9fb35
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-12 09:24:04 +0100

    Update index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,13 +1,11 @@
-function main(numOne, numTwo){
+function main(numOne, numTwo) {
 
     var quotient;
     
-    if(numTwo === 0){
-        quotient = Divide by 0! Cannot divide by zero! ;
+    if(numTwo === 0) {
+        quotient = Divide by 0! Cannot divide by zero!;
     } else {
-         // Only change code below this line 
-var numOne;
-var numTwo; 
+         // Only change code below this line  
 var quotient = numOne / numTwo; 
          // Only change code above this line 
 
@@ -19,4 +17,5 @@ console.log(main(15, 3));     //change this line
 console.log(main(25, 5));     //change this line 
 console.log(main(420, 10));    //change this line 
 console.log(main(55, 0));     //change this line 
+
 module.exports = main; 
